4eadcc93131cb3b0bb51f4150c2ecf761be05faf
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2022-09-09 17:17:59 -0300

    Formatting errors fixed

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -46,7 +46,8 @@ class ViewApp extends StatelessWidget {
       ],
       supportedLocales: AppLocalizations.supportedLocales,
       home: Navigator(
-        onPopPage: (Route<dynamic> route, dynamic result) => route.didPop(result),
+        onPopPage: (Route<dynamic> route, dynamic result) =>
+            route.didPop(result),
         pages: const [
           MaterialPage<void>(
             child: PageLogin(),
