eb8a8e5f65942f767cbea10d4e9e3daad19dbe27
NicoCieri10 <nicocieri16@gmail.com>
2022-09-26 20:58:45 -0300

    onboarding pages

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,8 +1,10 @@
+import 'package:appsize/appsize.dart';
 import 'package:auth_repository/auth_repository.dart';
 import 'package:data_persistence/data_persistence.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weappear/login/view/login_page.dart';
+import 'package:weappear/onboarding/view/onboarding_page.dart';
 import 'package:weappear_localizations/weappear_localizations.dart';
 import 'package:weappear_ui/weappear_ui.dart';
 
@@ -19,12 +21,21 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.light,
+        statusBarColor: Colors.transparent,
+      ),
+    );
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider.value(value: _authRepository),
         RepositoryProvider.value(value: _dataPersistenceRepository),
       ],
-      child: const ViewApp(),
+      child: AppSize.child(
+        child: const ViewApp(),
+      ),
     );
   }
 }
@@ -45,16 +56,11 @@ class ViewApp extends StatelessWidget {
       ],
       supportedLocales: AppLocalizations.supportedLocales,
       home: Navigator(
-        onPopPage: (Route<dynamic> route, dynamic result) =>
-            route.didPop(result),
+        onPopPage: (Route<dynamic> route, dynamic result) => route.didPop(result),
         pages: const [
           MaterialPage<void>(
-            child: PageLogin(),
-            key: ValueKey('login'),
-          ),
-          MaterialPage<void>(
-            child: PageLogin(),
-            key: ValueKey('login2asd'),
+            child: PageOnboarding(),
+            key: ValueKey('onboarding'),
           ),
         ],
       ),
