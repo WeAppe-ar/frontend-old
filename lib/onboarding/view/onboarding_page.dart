6e28aa6f4fb2486690e0f8e6dbe288efb8a36feb
NicoCieri10 <nicocieri16@gmail.com>
2022-09-29 23:45:04 -0300

    Onboarding done

--- lib/onboarding/view/onboarding_page.dart
+++ lib/onboarding/view/onboarding_page.dart
@@ -1,7 +1,7 @@
 import 'package:appsize/appsize.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 import 'package:weappear_ui/weappear_ui.dart';
 
 class PageOnboarding extends StatefulWidget {
@@ -12,98 +12,143 @@ class PageOnboarding extends StatefulWidget {
 }
 
 class _PageOnboardingState extends State<PageOnboarding> {
+  final PageController _pageController = PageController();
+  int index = 0;
+  final Map<int, String> map = {
+    0: '¡Contabilizá tus horas trabajadas en proyectos individuales o grupales!',
+    1: '¡Crea un proyecto e invitá a otros a participar!',
+    2: 'Unite a un proyecto ya armado y conectate con los participantes',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        index = _pageController.page!.round();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final pageDecoration = PageDecoration(
-      titleTextStyle: const TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-      contentMargin: EdgeInsets.zero,
-      bodyPadding: EdgeInsets.fromLTRB(65.sp, 30.sp, 65.sp, 0),
-      pageColor: Colors.white,
-      imagePadding: EdgeInsets.fromLTRB(20.sp, 0, 20.sp, 0),
-    );
-
-    return IntroductionScreen(
-      isTopSafeArea: true,
-      showNextButton: false,
-      showDoneButton: false,
-      globalBackgroundColor: Colors.white,
-      pages: [
-        PageViewModel(
-          title: '',
-          bodyWidget: const Text(
-            '¡Contabilizá tus horas trabajadas en proyectos individuales o grupales!',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ),
-          image: SvgPicture.asset(
-            'assets/images/onboarding_1.svg',
-            height: 300.sp,
-          ),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: '',
-          bodyWidget: const Text(
-            '¡Crea un proyecto e invitá a otros a participar!',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ),
-          image: SvgPicture.asset(
-            'assets/images/onboarding_2.svg',
-            height: 285.sp,
-          ),
-          decoration: pageDecoration,
-        ),
-        PageViewModel(
-          title: '',
-          bodyWidget: const Text(
-            'Unite a un proyecto ya armado y conectate con los participantes',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ),
-          image: SvgPicture.asset(
-            'assets/images/onboarding_3.svg',
-            height: 300.sp,
-          ),
-          decoration: pageDecoration,
-        ),
-      ],
-      globalFooter: Column(
+    return Scaffold(
+      body: Stack(
         children: [
-          SizedBox(
-            height: 180.sp,
+          PageView(
+            controller: _pageController,
+            children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: 130.sp,
+                  ),
+                  SizedBox(
+                    height: 294.sp,
+                    width: 337.sp,
+                    child: SvgPicture.asset(
+                      'assets/images/onboarding_1.svg',
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 130.sp,
+                  ),
+                  SizedBox(
+                    height: 295.sp,
+                    width: 352.sp,
+                    child: SvgPicture.asset(
+                      'assets/images/onboarding_2.svg',
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 130.sp,
+                  ),
+                  SizedBox(
+                    height: 298.sp,
+                    width: 338.sp,
+                    child: SvgPicture.asset(
+                      'assets/images/onboarding_3.svg',
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 38.sp, vertical: 68.sp),
-            child: Row(
+            padding: EdgeInsets.symmetric(vertical: 68.sp, horizontal: 38.sp),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                WeappearMaterialButton(
-                  height: 44.sp,
-                  minWidth: 149.sp,
-                  elevation: 0,
-                  color: Colors.transparent,
-                  fontColor: WeappearColors.blueActivated,
-                  onPressed: () {},
-                  title: 'Omitir',
+                DotsIndicator(
+                  dotsCount: 3,
+                  position: index.toDouble(),
+                  decorator: DotsDecorator(
+                    size: Size.square(11.sp),
+                    activeColor: const Color(0xff303030),
+                    activeSize: Size.square(11.sp),
+                    color: const Color(0xff9F9F9F),
+                    spacing: EdgeInsets.all(2.5.sp),
+                  ),
                 ),
-                WeappearMaterialButton(
-                  height: 44.sp,
-                  minWidth: 149.sp,
-                  elevation: 0,
-                  onPressed: () {},
-                  title: 'Siguiente',
+                SizedBox(
+                  height: 50.sp,
+                ),
+                SizedBox(
+                  width: 262.sp,
+                  height: 57.sp,
+                  child: Text(
+                    map[index]!,
+                    style: const TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 110.sp,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    WeappearMaterialButton(
+                      height: 44.sp,
+                      minWidth: 149.sp,
+                      elevation: 0,
+                      color: Colors.white,
+                      fontColor: WeappearColors.blueActivated,
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          2,
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.ease,
+                        );
+                      },
+                      title: 'Omitir',
+                    ),
+                    WeappearMaterialButton(
+                      height: 44.sp,
+                      minWidth: 149.sp,
+                      elevation: 0,
+                      onPressed: () {
+                        _pageController.nextPage(
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.ease,
+                        );
+                      },
+                      title: 'Siguiente',
+                    ),
+                  ],
                 ),
               ],
             ),
           ),
         ],
-      ),
-      dotsDecorator: DotsDecorator(
-        size: Size.square(11.sp),
-        activeColor: const Color(0xff303030),
-        activeSize: Size.square(11.sp),
-        color: const Color(0xff9F9F9F),
-        spacing: EdgeInsets.all(2.5.sp),
       ),
     );
   }
